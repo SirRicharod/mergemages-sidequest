46f8f8c7e82ebc987e14b7ae6f85a84df541ab9a
TheAbyssSage <sage.stockmans@pm.me>
2026-01-06 22:37:36 +0100

    Base post composer files

---
+++ angular-frontend/src/app/components/post-composer/post-composer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-composer',
+  imports: [],
+  templateUrl: './post-composer.html',
+  styleUrl: './post-composer.css',
+})
+export class PostComposer {
+
+}
