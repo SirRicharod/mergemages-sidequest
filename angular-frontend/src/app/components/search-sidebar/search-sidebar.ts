16037e8c5b8a25aec0ba1f0656e02e9cb4c2437a
TheAbyssSage <sage.stockmans@pm.me>
2026-01-07 13:03:35 +0100

    Added base files for right of feed search box

---
+++ angular-frontend/src/app/components/search-sidebar/search-sidebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-sidebar',
+  imports: [],
+  templateUrl: './search-sidebar.html',
+  styleUrl: './search-sidebar.css',
+})
+export class SearchSidebar {
+
+}
