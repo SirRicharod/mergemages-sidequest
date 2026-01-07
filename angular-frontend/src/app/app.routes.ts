30cfe666d1e2ec4e4f4360017a8881837c78bfd6
TheAbyssSage <sage.stockmans@pm.me>
2026-01-07 12:48:42 +0100

    Fixed routing in app.routes

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { HomeComponent } from './pages/home/home';
 import { Profile } from './pages/profile/profile';
 import { LoginRegistration } from './pages/login-registration/login-registration';
 
 export const routes: Routes = [
-    { path: '', component: Home, pathMatch: 'full' },
+    { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'profile', component: Profile },
     { path: 'login', component: LoginRegistration },
     { path: '**', redirectTo: '' }
