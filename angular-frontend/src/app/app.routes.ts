d04f2e35b26b5f79d37d579593086872567deea2
TheAbyssSage <sage.stockmans@pm.me>
2026-01-08 22:08:57 +0100

    Add routes for messages and quests components

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { Profile } from './pages/profile/profile';
 import { LoginRegistration } from './pages/login-registration/login-registration';
+import { Message } from './pages/message/message';
+import { Quests } from './pages/quests/quests';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'profile', component: Profile },
     { path: 'login', component: LoginRegistration },
+    { path: 'messages', component: Message },
+    { path: 'quests', component: Quests },
     { path: '**', redirectTo: '' }
 ];
