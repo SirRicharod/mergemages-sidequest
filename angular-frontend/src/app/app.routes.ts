2ca36a358742da4b6bc7b1e4bedd731ca9b6ad41
TheAbyssSage <sage.stockmans@pm.me>
2026-01-09 13:56:18 +0100

    Fixed feed problem

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
-import { Profile } from './pages/profile/profile';
+import { ProfileComponent } from './pages/profile/profile';
 import { LoginRegistration } from './pages/login-registration/login-registration';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'profile', component: Profile },
-    { path: 'login', component: LoginRegistration },
-    { path: '**', redirectTo: '' }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginRegistration },
+  { path: '**', redirectTo: '' }
 ];
