8d850a56e817ed8b954fe5532d9f00c134457752
TheAbyssSage <sage.stockmans@pm.me>
2026-01-10 20:10:50 +0100

    Enhance bottom-nav component with mode input and additional outputs

--- angular-frontend/src/app/components/bottom-nav/bottom-nav.ts
+++ angular-frontend/src/app/components/bottom-nav/bottom-nav.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+
+type Mode = 'requests' | 'offers';
 
 @Component({
   selector: 'app-bottom-nav',
@@ -10,7 +12,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrls: ['./bottom-nav.css']
 })
 export class BottomNavComponent {
+  @Input() mode: Mode = 'requests';
+  @Output() toggleMode = new EventEmitter<void>();
   @Output() makePost = new EventEmitter<void>();
+  @Output() openSearch = new EventEmitter<void>();
 
   onMakePost(): void {
     this.makePost.emit();
