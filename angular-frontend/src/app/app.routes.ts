411a512fa7dd9720c3c125ec13f3bdece502a9a0
TheAbyssSage <sage.stockmans@pm.me>
2026-01-10 20:11:04 +0100

    Fixed routing when in phone mode

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { ProfileComponent } from './pages/profile/profile';
 import { LoginRegistration } from './pages/login-registration/login-registration';
-import { Message } from './pages/message/message';
-import { Quests } from './pages/quests/quests';
+import { MessagesComponent } from './pages/message/message'; // adjust to your folder/file
+import { QuestsComponent } from './pages/quests/quests';       // adjust to your folder/file
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'profile', component: ProfileComponent },
-  { path: 'messages', component: Message },
-  { path: 'quests', component: Quests },
   { path: 'login', component: LoginRegistration },
+  { path: 'messages', component: MessagesComponent },
+  { path: 'quests', component: QuestsComponent },
   { path: '**', redirectTo: '' }
 ];
