39d6c432fe8feb242ea681cfafa6dae007c01b93
houssin-99 <houssinsyr@gmail.com>
2026-01-10 22:15:59 +0100

    Add Reviews component with routing and basic form

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './pages/home/home';
 import { ProfileComponent } from './pages/profile/profile';
 import { LoginRegistration } from './pages/login-registration/login-registration';
 import { MessagesComponent } from './pages/message/message'; // adjust to your folder/file
-import { QuestsComponent } from './pages/quests/quests';       // adjust to your folder/file
+import { QuestsComponent } from './pages/quests/quests';
+import { Reviews } from './reviews/reviews';       // adjust to your folder/file
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginRegistration },
   { path: 'messages', component: MessagesComponent },
   { path: 'quests', component: QuestsComponent },
+  { path: 'reviews', component: Reviews }, // New route for reviews
   { path: '**', redirectTo: '' }
 ];
