2494acc188aad4b46387cd1cd741a95ec422d521
TheAbyssSage <sage.stockmans@pm.me>
2026-01-10 22:16:24 +0100

    Added base files for search bar in phone-mode

---
+++ angular-frontend/src/app/components/mobile-search/mobile-search.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-search',
+  imports: [],
+  templateUrl: './mobile-search.html',
+  styleUrl: './mobile-search.css',
+})
+export class MobileSearch {
+
+}
